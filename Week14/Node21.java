f930322e461add88d170395dca2c41aa73dbf72f
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-06-07 19:18:41 +0700

    Node Class

---
+++ Week14/Node21.java
@@ -0,0 +1,16 @@
+package Week14;
+
+public class Node21 {
+    Student21 data;
+    Node21 left;
+    Node21 right;
+
+    Node21() {
+    }
+    
+    Node21(Student21 data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
